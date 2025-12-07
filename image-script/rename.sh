5bd6b621d95f103a86d7faa33be7b798e6294fc2
Ivan Kuchin <ivan.kuchin@gmail.com>
2025-12-07 11:30:39 -0500

    script wip

--- image-script/rename.sh
+++ image-script/rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find all jpg and png files and process them using xargs for efficiency
-find . -type f \( -iname "*.jpg" -o -iname "*.png" \) -print0 | while IFS= read -r -d '' input_file; do
+find . -type f \( -iname "*.jpg" -o -iname "*.png"  -o -iname "*.JPG"  -o -iname "*.PNG" \) -print0 | while IFS= read -r -d '' input_file; do
 
     extension="${input_file##*.}"
     # Get the source filename without the extension and without the path
@@ -12,14 +12,15 @@ find . -type f \( -iname "*.jpg" -o -iname "*.png" \) -print0 | while IFS= read 
 
     # Derive the output filename: lowercase and remove spaces and special characters
     dst_filename=$(echo "$src_filename" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9_-]/_/g')
+    output_file="${pathname}${dst_filename}.${extension}"
 
-    if [[ "$src_filename" == "$dst_filename" ]]; then
-        echo "Skipping: $input_file is already in the desired format."
+    if [[ "$input_file" == "$output_file" ]]; then
+        echo "Skipping: $input_file no need to rename."
         continue
     fi
 
-    echo "Renaming: $src_filename.$extension -> $dst_filename.$extension"
-    # mv "$input_file" "$dst_filename"
+    echo "Renaming: $input_file -> $output_file"
+    mv "$input_file" "$output_file"
 
 done
 
